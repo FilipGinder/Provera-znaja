1f97ff35deee4af73131421677525e8bee458223
Filip Ginder Ginder <ginderfilip@gmail.com>
2021-02-16 00:41:26 +0100

    16.02.2021

--- index.php
+++ index.php
@@ -2,6 +2,21 @@
 <html>
 <head>
 	<title>Provera znanja</title>
+	<!-- jquery -->
+	<script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
+<!-- jquery -->
+<!-- bootstrap -->
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
+<!-- bootstrap -->
+
+<!-- moje scripte -->
+<script src="src/js/script.js"></script>
+<!-- moje scripte -->
+<!-- moj css -->
+<link rel="stylesheet" type="text/css" href="src/css/style.css">
+<!-- moj css -->
+
 </head>
 <body>
 
