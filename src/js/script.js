e7ade2fe50c220904bee5ea07c8859321970b7df
Filip Ginder Ginder <ginderfilip@gmail.com>
2021-02-20 00:25:43 +0100

    20.02.2021

--- src/js/script.js
+++ src/js/script.js
@@ -1,12 +1,3 @@
 $(document).ready(function(){
-<<<<<<< HEAD
-alert('sa desktopa');
-<<<<<<< HEAD
-=======
-alert('sa gita');	
-  aaaa
->>>>>>> parent of 2fe6672... 2.16.2021
-=======
-alert('sa laptopa');
->>>>>>> parent of 1646899... 20.02.2021
+
 })
